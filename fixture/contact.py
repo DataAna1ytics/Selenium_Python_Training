3ba1e60fa26d590ae187476955fcfb045f40ef88
Margot222 <44867398+Margot222@users.noreply.github.com>
2018-12-26 18:07:16 +0300

    создан ContactHelper

---
+++ fixture/contact.py
@@ -0,0 +1,18 @@
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def add_new_contact(self, contact):
+        # add new contact
+        driver = self.app.driver
+        driver.find_element_by_link_text("add new").click()
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys(contact.firstname)
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys(contact.middlename)
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys(contact.lastname)
+        driver.find_element_by_name("nickname").clear()
+        driver.find_element_by_name("nickname").send_keys(contact.nickname)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,77 +1,17 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
 from model.contact import Contact
+from fixture.application import Application
+import pytest
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-    
-    def test_add_contact(self):
-        self.login()
-        self.add_new_contact(Contact("fst", "mdl", "lst", "nck"))
-        self.logout()
 
-    def logout(self):
-        # logout
-        driver = self.driver
-        driver.find_element_by_link_text("Logout").click()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def add_new_contact(self, contact):
-        # add new contact
-        driver = self.driver
-        driver.find_element_by_link_text("add new").click()
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(contact.firstname)
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(contact.middlename)
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(contact.lastname)
-        driver.find_element_by_name("nickname").clear()
-        driver.find_element_by_name("nickname").send_keys(contact.nickname)
 
-    def login(self):
-        # login
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_id("LoginForm").submit()
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-    
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-    
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.add_new_contact(Contact("fst", "mdl", "lst", "nck"))
+    app.session.logout()
