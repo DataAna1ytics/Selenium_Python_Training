d0e8de90df470ef55b637bda136562418988be3a
Margot222 <44867398+Margot222@users.noreply.github.com>
2019-01-03 18:25:13 +0300

    Revert "выполнение предусловий в тестах для удаления и модификации для групп и контактов"
    This reverts commit 3cc1071031eabe855572801d49676facd64cbaba.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,12 +2,8 @@ from model.contact import Contact
 
 
 def test_modify_contact_name(app):
-    if app.contact.count() == 0:
-        app.contact.add_new_contact(Contact("ftest", "mtest", "ltest", "ntest"))
     app.contact.modify_first_contact(Contact(firstname="New name"))
 
 
 #def test_modify_contact_lastname(app):
-#    if app.contact.count() == 0:
- #       app.contact.add_new_contact(Contact(firstname="test"))
 #    app.contact.modify_first_contact(Contact(name="New last name"))
