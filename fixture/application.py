0d432726b78f89e9889577ee14c48e5cf718d3e0
Margot222 <44867398+Margot222@users.noreply.github.com>
2019-01-12 00:36:01 +0300

    Передача параметров(браузер и url) в тесты из командной строки при запуске

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,20 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.driver = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.driver = webdriver.Firefox()
+        elif browser == "chrome":
+            self.driver = webdriver.Chrome()
+        elif browser == "ie":
+            self.driver = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognised browser %s" % browser)
         self.driver.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +32,7 @@ class Application:
     def open_home_page(self):
         # open homepage
         driver = self.driver
-        driver.get("http://localhost/addressbook/")
+        driver.get(self.base_url)
 
     def destroy(self):
         self.driver.quit()
